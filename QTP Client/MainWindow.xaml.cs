a6b483cdd4eae95f6db45f074bdf4d62d7cc8642
Good Boy <geodryaev1@gmail.com>
2022-05-13 10:49:36 +0300

    Push

---
+++ QTP Client/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SqlClient;
+using System.Windows;
+
+namespace QTP_Client
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void m_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SQLConnect form = new SQLConnect();
+            form.ShowDialog();
+        }
+
+        public string strSQLConnection()
+        {
+            return "Server=" + Properties.Settings.Default.pathSQL + ";Initial Catalog =QTPDB; User ID = sa; Password = qwerty12";
+        }
+
+        private void b_conncet_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connect = new SqlConnection(strSQLConnection()))
+                {
+                    connect.Open();
+                    connect.Close();
+                }
+                if(true)
+                {
+
+                }
+                //недостижимый потому что он вверзу всегда true
+                else
+                {
+                    MessageBox.Show("Введен неверный пароль");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка подключения к серверу, провертье путь к серверу" + Properties.Settings.Default.pathSQL); 
+            }
+        }
+    }
+}
---
+++ QTP Client/SQLConnect.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QTP_Client
+{
+    /// <summary>
+    /// Логика взаимодействия для SQLConnect.xaml
+    /// </summary>
+    public partial class SQLConnect : Window
+    {
+        public SQLConnect()
+        {
+            InitializeComponent();
+            tb_sqlPath.Text = Properties.Settings.Default.pathSQL;
+        }
+
+        private void b_go_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.pathSQL = tb_sqlPath.Text;
+            Properties.Settings.Default.Save();
+            Close();
+        }
+    }
+}
