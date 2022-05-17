70d9f4bf46de837ec43da91a6e448e56047c6fe1
Good Boy <geodryaev1@gmail.com>
2022-05-17 16:42:11 +0300

    Доваление тестирования

--- QTP Client/TestingWindow.xaml.cs
+++ QTP Client/TestingWindow.xaml.cs
@@ -11,15 +11,50 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+
 
 namespace QTP_Client
 {
 
     public partial class TestingWindow : Window
     {
-        public TestingWindow(string _unit, string _nameUnit, string _zvezda, string _fullName, string[] nameTems)
+        string[] nameTems;
+        string unit, numberUnit, zvezda, fullName;
+        public TestingWindow(string _unit, string _nameUnit, string _zvezda, string _fullName, string[] _nameTems)
         {
             InitializeComponent();
+            unit = _unit;
+            numberUnit = _nameUnit;
+            zvezda = _zvezda;
+            fullName = _fullName;
+            nameTems = _nameTems;
+            using (SqlConnection con = new SqlConnection(strSQLConnection()))
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand();
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM t_tems";
+
+            }
         }
+        public string strSQLConnection()
+        {
+            return "Server=" + Properties.Settings.Default.pathSQL + ";Initial Catalog =QTPDB; User ID = sa; Password = qwerty12";
+        }
+
+        public struct Disciplines
+        {
+            public string _name;
+            public string 
+        }
+        public struct Question
+        {
+            public string _nameQuestrion;
+            public string[] _answers;
+            public int _countAnser;
+            public string answer;
+        }
+
     }
 }
