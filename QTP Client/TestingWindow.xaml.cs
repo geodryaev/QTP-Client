cee75a12bc59eb6203cb55c5c67d3e938e1b866a
Good Boy <geodryaev1@gmail.com>
2022-06-27 17:01:06 +0300

    Ну чет сделал

--- QTP Client/TestingWindow.xaml.cs
+++ QTP Client/TestingWindow.xaml.cs
@@ -1,12 +1,26 @@
 ﻿using System.Data.SqlClient;
-using System.Windows;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
 
 namespace QTP_Client
 {
 
     public partial class TestingWindow : Window
     {
+        myAll all;
         string[] nameTems;
         string unit, numberUnit, zvezda, fullName;
         public TestingWindow(string _unit, string _nameUnit, string _zvezda, string _fullName, string[] _nameTems)
@@ -17,52 +31,84 @@ namespace QTP_Client
             zvezda = _zvezda;
             fullName = _fullName;
             nameTems = _nameTems;
-            using (SqlConnection con = new SqlConnection(strSQLConnection()))
-            {
-                con.Open();
-                SqlCommand com = new SqlCommand();
-                com.Connection = con;
-                com.CommandText = "SELECT * FROM t_tems";
-
-            }
-        }
-        public bool eqQuestion(string answer, string idQuestion)
-        {
-            using (SqlConnection con = new SqlConnection(strSQLConnection()))
-            {
-                con.Open();
-                SqlCommand com = new SqlCommand();
-                com.Connection = con;
-                com.CommandText = "";
-            }
-            return true;
+            all = new myAll(_nameTems, strSQLConnection());
+            Button b = new Button(), n = new Button();
+            b.Width = 200;
+            n.Width = 200;
+            b.Height = 200;
+            n.Height = 200;
+            sp_navigations.Children.Add(b);
+            sp_navigations.Children.Add(n);
+            
         }
 
 
 
 
+
+
+
+
+
+
+
+
+
+        //public bool eqQuestion(string answer, string idQuestion)
+        //{
+        //    using (SqlConnection con = new SqlConnection(strSQLConnection()))
+        //    {
+        //        con.Open();
+        //        SqlCommand com = new SqlCommand();
+        //        com.Connection = con;
+        //        com.CommandText = "";
+        //    }
+        //    return true;
+        //}
         public class myAll
         {
             public myAll(string[] nameDisciplines, string strSQLconnection)
             {
+                string n ="0";
                 _connectionSQL = strSQLconnection;
-            }
+                _arrayDisciplines = new Disciplines[nameDisciplines.Length];
+                for (int i = 0; i< nameDisciplines.Length; i++)
+                {
 
+                    using (SqlConnection con = new SqlConnection(_connectionSQL))
+                    {
+
+                        con.Open();
+                        SqlCommand com = new SqlCommand("SELECT * FROM t_tems");
+                        com.Connection = con;
+                        SqlDataReader read = com.ExecuteReader();
+                        while(read.Read())
+                        {
+                            if (nameDisciplines[i] == read.GetString(1).ToString())
+                            {
+                                n = read.GetString(5).ToString();
+                            }
+                        }
+                    }
+
+                    _arrayDisciplines[i] = new Disciplines(nameDisciplines[i], _connectionSQL, Convert.ToInt32(n));
+                }
+            }
             private string _connectionSQL;
-            public Disciplines[] _arrayQuestion;
+            public Disciplines[] _arrayDisciplines;
             public int _count;
         }
-
         public class Disciplines
         {
-            public Disciplines(string str, string strSQLconnection, int countQuestionTesting)
+            public Disciplines(string nameDis, string strSQLconnection, int countQuestionTesting)
             {
-                _nameDisciplines = str;
+                _nameDisciplines = nameDis;
                 _countQuestionTesting = countQuestionTesting;
                 _connection = new SqlConnection(strSQLconnection);
                 _connection.Open();
                 SqlCommand command = new SqlCommand();
                 command.CommandText = "SELECT * FROM t_tems";
+                command.Connection = _connection;
                 SqlDataReader read = command.ExecuteReader();
                 while (read.Read())
                 {
@@ -74,6 +120,7 @@ namespace QTP_Client
                 read.Close();
                 _connection.Close();
                 _arrayQuestion = new Question[_countQuestionTesting];
+                setQuestion();
             }
 
             private int countQuestion()
@@ -81,6 +128,7 @@ namespace QTP_Client
                 int count = 0;
                 _connection.Open();
                 SqlCommand com = new SqlCommand("SELECT * FROM t_question");
+                com.Connection = _connection;
                 SqlDataReader read = com.ExecuteReader();
                 while (read.Read())
                 {
@@ -93,28 +141,32 @@ namespace QTP_Client
                 _connection.Close();
                 return count;
             }
-            private void setQuestion ()
+            private void setQuestion()
             {
                 int count = 0;
-                string[] bufferArrayQuestion = new string[countQuestion()];
+                Question[] bufferArrayQuestion = new Question[countQuestion()];
                 _connection.Open();
                 SqlCommand com = new SqlCommand("SELECT * FROM t_question");
+                com.Connection= _connection;
                 SqlDataReader read = com.ExecuteReader();
                 while (read.Read())
                 {
                     if (read.GetValue(1).ToString() == _tKey)
                     {
-                        bufferArrayQuestion[count] = read.GetValue(0).ToString();
+                        bufferArrayQuestion[count]._nameQuestrion = read.GetValue(3).ToString();
+                        bufferArrayQuestion[count]._kQuestion = read.GetValue(0).ToString();
+                        bufferArrayQuestion[count]._kTrueAnswer = getArrayKTrueAnswer(read.GetValue(2).ToString());
                         count++;
                     }
                 }
                 read.Close();
                 int[] bufNubmerRandom = getArray(_countQuestionTesting);
+                _connection.Close();
                 for (int i = 0; i < _arrayQuestion.Length; i++)
                 {
-                    _arrayQuestion[i]._nameQuestrion = bufferArrayQuestion[randomNumber(ref bufNubmerRandom)];
+                    _arrayQuestion[i]= bufferArrayQuestion[randomNumber(ref bufNubmerRandom)];
+                    _arrayQuestion[i]._kAnswers = getArrayKAnswer(_arrayQuestion[i]._kQuestion);
                 }
-                _connection.Close();
             }
             private int randomNumber(ref int[] arrInt)
             {
@@ -142,22 +194,85 @@ namespace QTP_Client
 
                 return answer;
             }
-            
+            private string[] getArrayKTrueAnswer(string str)
+            {
+                if (str.Length > 0)
+                {
+                    int count = 0, indexArray = 0;
+                    string buffer = "";
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (str[i] == '|')
+                        {
+                            count++;
+                        }
+                    }
+                    string[] answer = new string[count];
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        buffer = "";
+                        while (i < str.Length && str[i] != '|')
+                        {
+                            buffer += str[i];
+                            i++;
+                        }
+                        if (buffer != "")
+                        {
+                            answer[indexArray] = buffer;
+                            indexArray++;
+                        }
+                    }
+                    return answer;
+                }
+                return null;
+            }
+            private string[] getArrayKAnswer(string qKey)
+            {
+                string[] str;
+                int _count = 0; 
+                _connection.Open();
+                SqlCommand com = new SqlCommand("SELECT * FROM t_answer");
+                com.Connection = _connection;
+                SqlDataReader read = com.ExecuteReader();
+                while(read.Read())
+                {
+                    if (qKey == read.GetValue(1).ToString())
+                        _count++;
+                }
+                str = new string[_count];
+                read.Close();
+                read = com.ExecuteReader();
+                _count = 0; 
+                while (read.Read())
+                {
+                    if (qKey == read.GetValue(1).ToString())
+                    {
+                        str[_count] = read.GetValue(0).ToString();
+                        _count++;
+                    }
+                }
+                _connection.Close();
+
+                return str;
+            }
+
+
             private int _countQuestionTesting;
-            private string _tKey ;
+            private string _tKey;
             public string _nameDisciplines;
             private SqlConnection _connection;
             public Question[] _arrayQuestion;
-        }
-        public struct Question
-        {
-            public string _nameQuestrion, _kQuestion;
-            public string[] _kAnswers;
-            public int[] _kTrueAnswer; //хранит индекс правильного ответа
         }
         public string strSQLConnection()
         {
             return "Server=" + Properties.Settings.Default.pathSQL + ";Initial Catalog =QTPDB; User ID = sa; Password = qwerty12";
         }
+        public struct Question
+        {
+            public string _nameQuestrion, _kQuestion;
+            public string[] _kAnswers;
+            public string[] _kTrueAnswer; //хранит индекс правильного ответа
+        }
+        
     }
 }
